02c42e1b5c1ce032ccd1fb3826751b7d7cff71df
Aikaterini Tsakkiri <katerinat@robotonio.gr>
2026-07-01 19:43:41 +0300

    Implement RGB control for temperature and humidity
    Added RGB control functions for temperature-based color indication.

---
+++ MODULES/rgb.py
@@ -0,0 +1,39 @@
+# IOT-RGB-PY
+from machine import Pin
+r =None
+g =None
+b =None
+
+r = Pin(14, Pin.OUT)
+g = Pin(13, Pin.OUT)
+b = Pin(12, Pin.OUT)
+ 
+def off():
+    r.value(0) # r.off()
+    g.value(0)
+    b.value(0)
+ 
+def on():
+    r.value(1) # r.on()
+    g.value(1)
+    b.value(1)
+ 
+def comfort_color(T, H):
+    if T < 17: # Cold
+        off()
+        b.value(1)
+        return "Cold  "
+    if T > 33 and H > 70: # Apopniktika
+        off()
+        r.value(1)
+        g.value(1)
+        return "Apopni"
+    if T > 35 and H < 70: # Hot
+        off()
+        r.value(1)
+        return "Hot   "
+    if T >= 17 and T <= 35: # Normal
+        off()
+        g.value(0)
+        b.value(1)
+        return "Normal"
--- README.md
+++
@@ -1,1 +0,0 @@
-# IOT-RGB-PY
